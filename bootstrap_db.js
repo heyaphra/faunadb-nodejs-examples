61328cfa1ebd292a0a8770af0be68d08292b9e6d
bloom510 <bloomjazzer@gmail.com>
2019-09-11 09:41:13 -0700

    Adds createSchema func

--- bootstrap_db.js
+++ bootstrap_db.js
@@ -13,8 +13,8 @@ const client = new faunadb.Client({ secret: process.env.FDB_ADMIN_KEY });
 
 const createDB = async () => {
     try {
-        const res = await client.query(q.CreateDatabase({ name: 'my_app' }))
-        console.log(res)
+        const _db = await client.query(q.CreateDatabase({ name: 'my_app' }))
+        console.log(_db)
     } catch (e) {
         console.log(e);
     }
@@ -25,6 +25,7 @@ const createKeys = async () => {
         let _server = await client.query(
             q.CreateKey({ database: q.Database('my_app'), role: 'server' })
         );
+        console.log('Successfully created server key')
         env.FDB_SERVER_KEY = _server.secret;
     } catch (e) {
         console.log(e)
@@ -33,6 +34,7 @@ const createKeys = async () => {
         let _client = await client.query(
             q.CreateKey({ database: q.Database('my_app'), role: 'client' })
         );
+        console.log('Successfully created client key')
         env.FDB_CLIENT_KEY = _client.secret;
     } catch (e) {
         console.log(e)
@@ -40,4 +42,28 @@ const createKeys = async () => {
     fs.writeFileSync('./.env', envfile.stringifySync(env));
 }
 
-(async () => { await createDB(); await createKeys(); })()
+const createSchema = async () => {
+    try {
+        const _users = await client.query(q.CreateCollection({ name: 'users' }))
+        let _index = await client.query(
+            q.CreateIndex(
+                {
+                    name: 'users_by_email',
+                    permissions: { read: 'public' },
+                    source: q.Collection('users'),
+                    terms: [{ field: ['data', 'email'] }],
+                    unique: true
+                }
+            )
+        )
+        console.log(_users, _index)
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+(async () => {
+    await createDB();
+    await createKeys();
+    await createSchema();
+})()
