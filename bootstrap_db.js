1abd90776e71290ece25049a2640d30c05a01a9c
bloom510 <bloomjazzer@gmail.com>
2019-09-11 12:22:50 -0700

    Adds complete bootstrap script

--- bootstrap_db.js
+++ bootstrap_db.js
@@ -1,3 +1,15 @@
+/** 
+ * Summary: 
+ * This script creates a new database testdb with the specified schema and test entry.
+ * 
+ * Prerequisites: 
+ * You must have an FDB_ADMIN_KEY in your .env file. This script takes care of the rest
+ * by writing the client and server keys for the new database in your .env file.
+ * 
+ * TODO: Make into an interactive shell program to add flexibility, prevent overwrites, etc.
+ *  
+ */
+
 require('dotenv').config();
 
 const fs = require('fs');
@@ -9,40 +21,41 @@ const env = envfile.parseFileSync('.env');
 const faunadb = require('faunadb'),
     q = faunadb.query;
 
-const client = new faunadb.Client({ secret: process.env.FDB_ADMIN_KEY });
+let client = new faunadb.Client({ secret: process.env.FDB_ADMIN_KEY });
 
 const createDB = async () => {
     try {
-        const _db = await client.query(q.CreateDatabase({ name: 'my_app' }))
-        console.log(_db)
+        const _db = await client.query(q.CreateDatabase({ name: 'testdb' }))
     } catch (e) {
         console.log(e);
     }
 }
 
 const createKeys = async () => {
+    let _server, _client;
     try {
-        let _server = await client.query(
-            q.CreateKey({ database: q.Database('my_app'), role: 'server' })
+        _server = await client.query(
+            q.CreateKey({ database: q.Database('testdb'), role: 'server' })
         );
-        console.log('Successfully created server key')
+        console.log('Created server key')
         env.FDB_SERVER_KEY = _server.secret;
     } catch (e) {
         console.log(e)
     }
     try {
-        let _client = await client.query(
-            q.CreateKey({ database: q.Database('my_app'), role: 'client' })
+        _client = await client.query(
+            q.CreateKey({ database: q.Database('testdb'), role: 'client' })
         );
-        console.log('Successfully created client key')
+        console.log('Created client key')
         env.FDB_CLIENT_KEY = _client.secret;
     } catch (e) {
         console.log(e)
     }
-    fs.writeFileSync('./.env', envfile.stringifySync(env));
+    await fs.writeFile('./.env', envfile.stringifySync(env), () => console.log('Wrote env vars'));
+    return { server: _server.secret, client: _client.secret };
 }
 
-const createSchema = async () => {
+const createSchema = async function (client) {
     try {
         const _users = await client.query(q.CreateCollection({ name: 'users' }))
         let _index = await client.query(
@@ -56,14 +69,42 @@ const createSchema = async () => {
                 }
             )
         )
-        console.log(_users, _index)
+        console.log('Created schema')
     } catch (e) {
         console.log(e);
     }
 }
 
-(async () => {
+const createUsers = async function (client) {
+    try {
+        const _user = await client.query(
+            q.Create(
+                q.Collection('users'),
+                {
+                    credentials: { password: 'secret password' },
+                    data: { email: 'alice@example.com' }
+                }
+            )
+        )
+        console.log('Created a test user', _user)
+
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+const setupDB = async () => {
     await createDB();
-    await createKeys();
-    await createSchema();
+    const _keys = await createKeys();
+    return {
+        client: new faunadb.Client({ secret: _keys.server }),
+        keys: _keys
+    }
+}
+
+(async () => {
+    const db = await setupDB();
+    const server_client = db.client;
+    await createSchema(server_client);
+    await createUsers(server_client);
 })()
