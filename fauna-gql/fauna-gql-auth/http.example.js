8f19f49f8ab2e2cb34a7d707db5c61ccbdbd9491
bloom510 <bloomjazzer@gmail.com>
2019-09-13 14:36:01 -0700

    Re-organizes things

---
+++ fauna-gql/fauna-gql-auth/http.example.js
@@ -0,0 +1,26 @@
+/**
+ * Summary: 
+ *   - Basic example showing HTTP request to a Fauna endpoint
+ * 
+ * Notes:
+ *  - All HTTP requests must be authenticated with a base-64 digest of your relevant key
+ *  - For GraphQL you may use a Bearer authentication scheme
+ */
+
+const axios = require("axios");
+
+const example = async token => {
+  const digest = Buffer.from(`${token}:`).toString("base64");
+  const opts = {
+    method: '<GET | POST | PUT | DELETE>',
+    headers: { Authorization: `Basic ${digest}` },
+    url: '<fauna endpoint>'
+  };
+  try {
+    const _res = await axios(opts);
+    console.log(_res.status);
+  } catch (e) {
+    throw new Error(e);
+  }
+};
+
---
+++ fauna-gql/fauna-gql-auth/index.js
@@ -0,0 +1,80 @@
+/**
+ * Summary:
+ * This script contains functions that use our mock data to perform some common authentication related tasks.
+ * 
+ * Prerequisites:
+ * You must have a seeded FaunaDB created using ../bootstrap_db
+ * 
+ */
+
+require('dotenv').config();
+
+const faunadb = require('faunadb');
+
+const { Login, Logout, Select, Get, Match, Identify, Index } = faunadb.query;
+
+let client = new faunadb.Client({ secret: process.env.FDB_SERVER_KEY });
+
+const loginWithEmail = async (email, password) => {
+    try {
+        const _user = await findUserByEmail(email);
+        const _res = await client.query(
+            Login(
+                Match(
+                    Index('users_by_email'),
+                    Select(['data', 'email'], Get(_user))
+                ), { password }
+            )
+        );
+        console.log(_res)
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+const findUserByEmail = async (email) => {
+    try {
+        const _res = await client.query(
+            Select(['ref'], Get(Match(Index('users_by_email'), email)))
+        );
+        return _res;
+    } catch (e) {
+        console.log(e.status)
+    }
+}
+
+const validateCredentials = async (user, password) => {
+    try {
+        const _id = await client.query(
+            Identify(Match(Index('users_by_email'), user), password)
+        );
+        console.log(_id);
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+const logout = async () => {
+    /**
+     * Client must use token generated via login.
+     * It's up to you to store it securely.  
+     * */
+    try {
+        const _res = await client.query(Logout(true));
+        console.log(_res)
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+/* Login */
+// loginWithEmail('alice@example.com', 'secret password')
+
+/* Find user by email */
+// findUserByEmail('alice@example.com')
+
+/* Logout */
+// logout()
+
+/* Get auth status */
+// validateCredentials('alice@example.com', 'secret_password')
