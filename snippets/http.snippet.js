558c170d8b4f753fe405b30209ec3b715270c7b6
Natalie Bloomfield <31744764+bloom510@users.noreply.github.com>
2019-09-13 14:45:47 -0700

    Update http.snippet.js

--- snippets/http.snippet.js
+++ snippets/http.snippet.js
@@ -4,7 +4,7 @@
  * 
  * Notes:
  *  - All HTTP requests must be authenticated with a base-64 digest of your relevant key
- *  - For GraphQL you may use a Bearer authentication scheme
+ *  - For GraphQL you may use a Bearer authentication scheme and completely forego base-64 encoding
  */
 
 const axios = require("axios");
