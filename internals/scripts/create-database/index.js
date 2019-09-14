fbe552c4eb51540f1a75ee190ba96df338e42c90
bloom510 <bloomjazzer@gmail.com>
2019-09-13 17:56:14 -0700

    Adds prompts

--- internals/scripts/create-database/index.js
+++ internals/scripts/create-database/index.js
@@ -1,11 +1,8 @@
 /**
  * This will be a CLI script that lets users provision Fauna database(s)
- *
+ * WARNING: THIS IS NOT READY FOR USE. CURRENTLY UNDER DEVELOPMENT.
  */
-
-const minimist = require("minimist");
-
-module.exports = () => {
-  const args = minimist(process.argv.slice(2));
-  console.log(args, 'DB!');
+const { Walkthrough } = require('./main');;
+module.exports = async () => {
+  const questions = new Walkthrough().askQuestions()
 };
