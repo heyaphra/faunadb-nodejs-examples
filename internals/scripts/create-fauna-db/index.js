ceb3f7bd3364ce2bc91b5017840f04d276cb8e15
bloom510 <bloomjazzer@gmail.com>
2019-09-13 20:06:41 -0700

    Moves env logic to func

--- internals/scripts/create-fauna-db/index.js
+++ internals/scripts/create-fauna-db/index.js
@@ -3,14 +3,19 @@
  * WARNING: THIS IS NOT READY FOR USE. CURRENTLY UNDER DEVELOPMENT.
  */
 const fs = require("fs");
+const envfile = require("envfile");
 const appRoot = require("app-root-path");
 const { Walkthrough } = require("./main");
+
 module.exports = async () => {
-  const envfile = require("envfile");
+  checkEnv();
+  new Walkthrough().askQuestions();
+};
+
+const checkEnv = () => {
   const envPath = appRoot + "/.env";
-  let env = fs.existsSync(envPath);
+  const env = fs.existsSync(envPath);
   if (!env) {
     fs.writeFileSync(envPath);
   }
-  new Walkthrough().askQuestions();
-};
+}
