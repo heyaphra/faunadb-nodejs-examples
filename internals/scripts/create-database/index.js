22207947d74a23078d9a3db1cee32e57e1a3434e
bloom510 <bloomjazzer@gmail.com>
2019-09-13 18:01:20 -0700

    Adds ora

--- internals/scripts/create-database/index.js
+++ internals/scripts/create-database/index.js
@@ -2,7 +2,8 @@
  * This will be a CLI script that lets users provision Fauna database(s)
  * WARNING: THIS IS NOT READY FOR USE. CURRENTLY UNDER DEVELOPMENT.
  */
-const { Walkthrough } = require('./main');;
+const ora = require('ora');
+const { Walkthrough } = require('./main');
 module.exports = async () => {
   const questions = new Walkthrough().askQuestions()
 };
